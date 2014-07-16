09737c795c17ff88244f0661a5c6506f1198e9e4
Timothy Klenke <timothy.klenke@gmail.com>
2014-07-15 21:30:39 -0600

    Basic garbage detection

--- StronglyTypedEnumConverterLib/Converter.cs
+++ StronglyTypedEnumConverterLib/Converter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.CodeDom.Compiler;
+using System.Reflection;
 
 namespace StronglyTypedEnumConverter
 {
@@ -9,7 +8,29 @@ namespace StronglyTypedEnumConverter
     {
         public string Convert(string clrEnumDef)
         {
+            var clrEnum = CompileCode(clrEnumDef);
+
             return string.Empty;
         }
+
+
+        private static Assembly CompileCode(string sourceCode)
+        {
+            var compiler = new Microsoft.CSharp.CSharpCodeProvider();
+            var parameters = new CompilerParameters
+            {
+                GenerateInMemory = true,
+                GenerateExecutable = false
+            };
+
+            var compilerOut = compiler.CompileAssemblyFromSource(parameters, sourceCode);
+
+            if (compilerOut.Errors.Count == 0)
+                return compilerOut.CompiledAssembly;
+
+            throw new ArgumentException("Could Not Compile Code.  There were " + compilerOut.Errors.Count +
+                                           " errors.  The first was " + compilerOut.Errors[0]);
+        }
+
     }
 }
--- StronglyTypedEnumConverter_Tests/ConverterTests.cs
+++ StronglyTypedEnumConverter_Tests/ConverterTests.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace StronglyTypedEnumConverter
@@ -22,10 +26,56 @@ namespace StronglyTypedEnumConverter
             var converter = new Converter();
             var output = converter.Convert(CowboyTypeEnumDef);
             Assert.IsNotNull(output);
-
+        }
+        
+        [TestMethod]
+        public void Converter_BasicEnum_ReturnsCodeThatCompiles()
+        {
+            var converter = new Converter();
+            var sourceCode = converter.Convert(CowboyTypeEnumDef);
+            
+            var assembly = CompileCode(sourceCode);
+            Assert.IsNotNull(assembly);
         }
 
-    
+        private static Assembly CompileCode(string sourceCode)
+        {
+            var compiler = new Microsoft.CSharp.CSharpCodeProvider();
+            var parameters = new CompilerParameters
+            {
+                GenerateInMemory = true,
+                GenerateExecutable = false
+            };
+
+            var compilerOut = compiler.CompileAssemblyFromSource(parameters, sourceCode);
+
+            if (compilerOut.Errors.Count == 0) 
+                return compilerOut.CompiledAssembly;
+
+            foreach (var error in compilerOut.Errors)
+                Console.Error.WriteLine(error.ToString());
+            throw new ApplicationException("Could Not Compile Code");
+        }
+
+        [TestMethod]
+        public void Converter_GarbageIn_ThrowsArgumentException()
+        {
+            var converter = new Converter();
+            try
+            {
+                converter.Convert("garbage }{");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex);
+                return;
+            }
+
+            Assert.Fail("Expected exception did not occur");            
+        }
+
+
+
     }
     
 }

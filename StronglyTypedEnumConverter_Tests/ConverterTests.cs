cffae0eca044780e14af3fa62ff23a5532744b39
Timothy Klenke <timothy.klenke@gmail.com>
2014-07-15 20:54:22 -0600

    Initial commit - nearly empty project

---
+++ StronglyTypedEnumConverter_Tests/ConverterTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StronglyTypedEnumConverter
+{
+    [TestClass]
+    public class ConverterTests
+    {
+        [TestMethod]
+        public void Converter_Initialize_ObjectCreated()
+        {
+            var converter = new Converter();
+            Assert.IsNotNull(converter);
+        }
+    }
+}

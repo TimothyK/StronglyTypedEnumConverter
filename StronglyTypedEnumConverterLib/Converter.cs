7a1a510cecf7a2f2eead98d11301dc105578b290
Timothy Klenke <timothy.klenke@gmail.com>
2014-07-15 21:05:06 -0600

    Empty Convert method

--- StronglyTypedEnumConverterLib/Converter.cs
+++ StronglyTypedEnumConverterLib/Converter.cs
@@ -7,5 +7,9 @@ namespace StronglyTypedEnumConverter
 {
     public class Converter
     {
+        public string Convert(string clrEnumDef)
+        {
+            return string.Empty;
+        }
     }
 }
--- StronglyTypedEnumConverter_Tests/ConverterTests.cs
+++ StronglyTypedEnumConverter_Tests/ConverterTests.cs
@@ -12,5 +12,20 @@ namespace StronglyTypedEnumConverter
             var converter = new Converter();
             Assert.IsNotNull(converter);
         }
+
+
+        private const string CowboyTypeEnumDef = "enum CowboyType {Good,Bad,Ugly};";        
+
+        [TestMethod]
+        public void Converter_BasicEnum_ReturnsNonEmptyValue()
+        {
+            var converter = new Converter();
+            var output = converter.Convert(CowboyTypeEnumDef);
+            Assert.IsNotNull(output);
+
+        }
+
+    
     }
+    
 }

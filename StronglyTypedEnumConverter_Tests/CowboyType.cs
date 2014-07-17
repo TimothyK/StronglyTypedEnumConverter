f8d87d44706ea58bedc4d54e43e5136b23ad25fc
Timothy Klenke <timothy.klenke@gmail.com>
2014-07-16 22:50:34 -0600

    Added an example to the tests of the expected final output of the enum converter

---
+++ StronglyTypedEnumConverter_Tests/CowboyType.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StronglyTypedEnumConverter
+{
+    class CowboyType
+    {
+        private CowboyType() 
+        {
+        }
+
+        public static readonly CowboyType Good = new CowboyType();
+        public static readonly CowboyType Bad = new CowboyType();
+        public static readonly CowboyType Ugly = new CowboyType();
+
+        public static IEnumerable<CowboyType> All()
+        {
+            yield return Good;
+            yield return Bad;
+            yield return Ugly;
+        }
+
+        public override string ToString()
+        {
+            var map = new Dictionary<CowboyType, string>
+            {
+                {Good, "Good"},
+                {Bad, "Bad"},
+                {Ugly, "Ugly"}
+            };
+
+            return map[this];
+        }
+
+        public static CowboyType FromString(string value)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+
+            var result = All().FirstOrDefault(x => x.ToString() == value);
+            if (result != null) return result;
+
+            throw new ArgumentOutOfRangeException("value", value, "Invalid CowboyType");
+        }
+
+        public static explicit operator int(CowboyType value)
+        {
+            var map = new Dictionary<CowboyType, int>
+            {
+                {Good, 0},
+                {Bad, 1},
+                {Ugly, 2}
+            };
+
+            return map[value];            
+        }
+
+        public static explicit operator CowboyType(int value)
+        {
+            var result = All().FirstOrDefault(x => (int) x == value);
+            if (result != null) return result;
+
+            throw new InvalidCastException("The value " + value + " is not a valid CowboyType");
+        }
+
+    }
+}
---
+++ StronglyTypedEnumConverter_Tests/CowboyTypeTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StronglyTypedEnumConverter
+{
+    [TestClass]
+    public class CowboyTypeTests
+    {
+        [TestMethod]
+        public void CowboyType_FromInvalidString_ThrowArgRange()
+        {
+            try
+            {
+                CowboyType.FromString("Jolly");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+
+            Assert.Fail("Expected exception did not occur");
+            
+        }
+        
+        [TestMethod]
+        public void CowboyType_FromNullString_ThrowArgNull()
+        {
+            try
+            {
+                CowboyType.FromString(null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+
+            Assert.Fail("Expected exception did not occur");
+            
+        }
+
+        [TestMethod]
+        public void CowboyType_All_ReturnsAllThreeValues()
+        {
+            var types = CowboyType.All().ToArray();
+
+            Assert.IsTrue(types.Contains(CowboyType.Good));
+            Assert.IsTrue(types.Contains(CowboyType.Bad));
+            Assert.IsTrue(types.Contains(CowboyType.Ugly));
+        }
+
+        [TestMethod]
+        public void CowboyType_StringValuesRoundTrip_ReturnsOriginalValue()
+        {
+            foreach (var type in CowboyType.All())
+                Assert.AreSame(type, CowboyType.FromString(type.ToString()), type + " did not round trip successfully");
+        }
+
+        [TestMethod]
+        public void CowboyType_CastToInt_ReturnsExpected()
+        {
+            Assert.AreEqual(0, (int) CowboyType.Good);
+            Assert.AreEqual(1, (int) CowboyType.Bad);
+            Assert.AreEqual(2, (int) CowboyType.Ugly);
+        }
+        
+        [TestMethod]
+        public void CowboyType_CastFromInt_ReturnsExpected()
+        {
+            Assert.AreEqual(CowboyType.Good, (CowboyType) 0);
+            Assert.AreEqual(CowboyType.Bad, (CowboyType) 1);
+            Assert.AreEqual(CowboyType.Ugly, (CowboyType) 2);
+        }
+
+        [TestMethod]
+        public void CowboyType_FromInvalidInt_ThrowInvalidCast()
+        {
+            try
+            {
+                var dummy = (CowboyType) 4;
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+
+            Assert.Fail("Expected exception did not occur");
+
+        }
+
+    }
+}

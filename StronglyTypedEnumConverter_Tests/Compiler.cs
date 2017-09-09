98a0aefd2b86f138e6fe3e44a2e2af6cc9bb49a3
Timothy Klenke <timothy.klenke@gmail.com>
2017-09-08 22:26:56 -0600

    #5b-minor-Remove dead code, Resharper recomendations

--- StronglyTypedEnumConverter_Tests/Compiler.cs
+++ StronglyTypedEnumConverter_Tests/Compiler.cs
@@ -1,25 +1,14 @@
 ﻿using System;
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Emit;
 
 namespace StronglyTypedEnumConverter
 {
     internal class Compiler
     {
-        public Compiler()
-        {
-            References.Add("System.dll");
-            References.Add("System.Core.dll");
-        }
-
-        public readonly List<string> References = new List<string>();
-
         /// <summary>
         /// Compiles the source code into an in-memory Assembly
         /// </summary>
@@ -28,74 +17,41 @@ namespace StronglyTypedEnumConverter
         public Assembly Compile(string sourceCode)
         {
 
-            string assemblyName = Path.GetRandomFileName();
+            var assemblyName = Path.GetRandomFileName();
 
-            MetadataReference[] references = new MetadataReference[]
-            {
+            MetadataReference[] references = {
                 MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
                 MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location),
             };
 
-            //var assemblyPath = Path.GetDirectoryName(typeof(object).Assembly.Location);
-            //MetadataReference[] references = new MetadataReference[]
-            //{
-            //    MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "mscorlib.dll")),
-            //    MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "System.dll")),
-            //    MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "System.Core.dll")),
-            //    MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "System.Runtime.dll")),
-            //};
-
             var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
-            CSharpCompilation compilation = CSharpCompilation.Create(
+            var compilation = CSharpCompilation.Create(
                 assemblyName,
-                syntaxTrees: new[] { syntaxTree },
-                references: references,
-                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-
-
+                new[] { syntaxTree },
+                references,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
 
             using (var ms = new MemoryStream())
             {
-                EmitResult result = compilation.Emit(ms);
+                var result = compilation.Emit(ms);
 
                 if (result.Success)
                 {
                     ms.Seek(0, SeekOrigin.Begin);
-                    Assembly assembly = Assembly.Load(ms.ToArray());
+                    var assembly = Assembly.Load(ms.ToArray());
                     return assembly;
                 }
 
-                IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                var failures = result.Diagnostics.Where(diagnostic =>
                     diagnostic.IsWarningAsError ||
                     diagnostic.Severity == DiagnosticSeverity.Error);
 
-                foreach (Diagnostic diagnostic in failures)
+                foreach (var diagnostic in failures)
                 {
                     Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
                 }
                 throw new ApplicationException("Could Not Compile Code");
             }
-
-
-
-
-            var compiler = new Microsoft.CSharp.CSharpCodeProvider();
-            var parameters = new CompilerParameters
-            {
-                GenerateInMemory = true,
-                GenerateExecutable = false
-            };
-            foreach (var reference in References)
-                parameters.ReferencedAssemblies.Add(reference);
-
-            var compilerOut = compiler.CompileAssemblyFromSource(parameters, sourceCode);
-
-            if (compilerOut.Errors.Count == 0)
-                return compilerOut.CompiledAssembly;
-
-            foreach (var error in compilerOut.Errors)
-                Console.Error.WriteLine(error.ToString());
-            throw new ApplicationException("Could Not Compile Code");
         }
     }
 }
